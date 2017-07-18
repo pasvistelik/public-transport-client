aaf4e4870f7d8f50b940a341c621484bc8a3f469
pasvistelik <pasvistelik@gmail.com>
2017-07-18 18:26:54 +0300

    Add new API server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    apiPublicTransportServer: "https://ptp-97126.app.xervo.io/",
+    apiPublicTransportServer: "https://public-transport-api-server.herokuapp.com/",
     apiGetStationsUrl: "https://publictransportproject.000webhostapp.com/new/json/stations.json", //"stations"
     apiGetRoutesUrl: "https://publictransportproject.000webhostapp.com/new/json/routes.json", //"routes"
     apiGetTimetablesUrl: "https://publictransportproject.000webhostapp.com/new/json/timetables.json", //"timetables"
