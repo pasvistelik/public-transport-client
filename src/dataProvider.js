0d06b7467bb92695d8619ff9134de993e1a81044
pasvistelik <pasvistelik@gmail.com>
2017-07-27 18:56:37 +0300

    Optimize code.

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -2,128 +2,111 @@ import JsonDataStorage from './jsonDataStorage';
 import initialize from 'public-transport-initialize-data';
 import ApiConfig from './config';
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Load data.
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+export default class DataProvider {
+    static allStations = null;
+    static allRoutes = null;
+    static allTimetables = null;
 
-var allStations = null;
-var allRoutes = null;
-var allTimetables = null;
+    //static updatingFromServerInterval = 5000;
 
-//var updatingFromServerInterval = 5000;
+    static loadingStarted = false;
 
-var loadingStarted = false;
+    static allStationsLoaded = false;
+    static allRoutesLoaded = false;
+    static allTimetablesLoaded = false;
+    static allStationsJSON = null;
+    static allRoutesJSON = null;
+    static allTimetablesJSON = null;
 
-var allStationsLoaded = false, allRoutesLoaded = false,  allTimetablesLoaded = false;
-var allStationsJSON = null, allRoutesJSON = null, allTimetablesJSON = null;
-
-async function loadDataOnly() {
-    allStationsJSON = await JsonDataStorage.getAllStations();
-    allRoutesJSON = await JsonDataStorage.getAllRoutes();
-    allTimetablesJSON = await JsonDataStorage.getAllTimetables();
-
-    if(!allStationsLoaded){
-        if (allStationsJSON == null) { //localStorage["allStationsJSON"] === undefined || localStorage["allStationsJSON"] == null
-            console.log("Downloading stations from server...");
-
-            let response = await fetch(ApiConfig.apiGetStationsUrl);
-            allStationsJSON = await response.text();
-            allStations = JSON.parse(allStationsJSON);//await response.json();
-
-            if (allStations !== undefined && allStations != null) JsonDataStorage.pushAllStations(allStationsJSON); //localStorage["allStationsJSON"] = allStationsJSON;
-            allStationsLoaded = true;
-            console.log("Stations loaded from server.");
-        }
-        else {
-            allStations = JSON.parse(allStationsJSON); //localStorage["allStationsJSON"]
-            allStationsLoaded = true;
-            console.log("Stations loaded from localStorage.");
-        }
-    }
-
-    if(!allRoutesLoaded){
-        if (allRoutesJSON == null) { //localStorage["allRoutesJSON"] === undefined || localStorage["allRoutesJSON"] == null
-            console.log("Downloading routes from server...");
-
-            let response = await fetch(ApiConfig.apiGetRoutesUrl);
-            allRoutesJSON = await response.text();
-            allRoutes = JSON.parse(allRoutesJSON);//await response.json();
-
-            if (allRoutes !== undefined && allRoutes != null) JsonDataStorage.pushAllRoutes(allRoutesJSON); //localStorage["allRoutesJSON"] = allRoutesJSON;
-            allRoutesLoaded = true;
-            console.log("Routes loaded from server.");
-        }
-        else {
-            allRoutes = JSON.parse(allRoutesJSON); //localStorage["allRoutesJSON"]
-            allRoutesLoaded = true;
-            console.log("Routes loaded from localStorage.");
-        }
-    }
-
-    if(!allTimetablesLoaded){
-        if (allTimetablesJSON == null) { //localStorage["allTimetablesJSON"] === undefined || localStorage["allTimetablesJSON"] == null
-            console.log("Downloading timetables from server...");
-
-            let response = await fetch(ApiConfig.apiGetTimetablesUrl);
-            allTimetablesJSON = await response.text();
-            allTimetables = JSON.parse(allTimetablesJSON);//await response.json();
-
-            if (allTimetables !== undefined && allTimetables != null) JsonDataStorage.pushAllTimetables(allTimetablesJSON); //localStorage["allTimetablesJSON"] = allTimetablesJSON;
-            allTimetablesLoaded = true;
-            console.log("Timetables loaded from server.");
-        }
-        else {
-            allTimetables = JSON.parse(allTimetablesJSON); //localStorage["allTimetablesJSON"]
-            allTimetablesLoaded = true;
-            console.log("Timetables loaded from localStorage.");
-        }
-    }
-}
-
-async function loadData() {
-    if(!loadingStarted){
-        loadingStarted = true;
-
-        await loadDataOnly();
-
-        if (allStationsLoaded && allRoutesLoaded && allTimetablesLoaded) {
-            initialize(allStations, allRoutes, allTimetables);
-        }
-    }
-}
-
-
-//loadData();
-
-class DataProvider {
     static getAllStations() {
-        return allStations;
+        return DataProvider.allStations;
     }
     static getAllRoutes() {
-        return allRoutes;
+        return DataProvider.allRoutes;
     }
     static getAllTimetables() {
-        return allTimetables;
+        return DataProvider.allTimetables;
     }
     static getAllStationsJSON() {
-        return allStationsJSON;
+        return DataProvider.allStationsJSON;
     }
     static getAllRoutesJSON() {
-        return allRoutesJSON;
+        return DataProvider.allRoutesJSON;
     }
     static getAllTimetablesJSON() {
-        return allTimetablesJSON;
+        return DataProvider.allTimetablesJSON;
     }
     static async loadDataAndInitialize() {
-        await loadData();
+        if(!DataProvider.loadingStarted){
+            DataProvider.loadingStarted = true;
+
+            await DataProvider.loadDataOnly();
+
+            if (DataProvider.allStationsLoaded && DataProvider.allRoutesLoaded && DataProvider.allTimetablesLoaded) {
+                initialize(DataProvider.allStations, DataProvider.allRoutes, DataProvider.allTimetables);
+            }
+        }
     }
     static async loadDataOnly() {
-        await loadDataOnly();
+        DataProvider.allStationsJSON = await JsonDataStorage.getAllStations();
+        DataProvider.allRoutesJSON = await JsonDataStorage.getAllRoutes();
+        DataProvider.allTimetablesJSON = await JsonDataStorage.getAllTimetables();
+
+        if(!DataProvider.allStationsLoaded){
+            if (DataProvider.allStationsJSON == null) {
+                console.log("Downloading stations from server...");
+
+                let response = await fetch(ApiConfig.apiGetStationsUrl);
+                DataProvider.allStationsJSON = await response.text();
+                DataProvider.allStations = JSON.parse(DataProvider.allStationsJSON);
+
+                if (DataProvider.allStations !== undefined && DataProvider.allStations != null) JsonDataStorage.pushAllStations(DataProvider.allStationsJSON);
+                DataProvider.allStationsLoaded = true;
+                console.log("Stations loaded from server.");
+            }
+            else {
+                DataProvider.allStations = JSON.parse(DataProvider.allStationsJSON);
+                DataProvider.allStationsLoaded = true;
+                console.log("Stations loaded from localStorage.");
+            }
+        }
+
+        if(!DataProvider.allRoutesLoaded){
+            if (DataProvider.allRoutesJSON == null) {
+                console.log("Downloading routes from server...");
+
+                let response = await fetch(ApiConfig.apiGetRoutesUrl);
+                DataProvider.allRoutesJSON = await response.text();
+                DataProvider.allRoutes = JSON.parse(DataProvider.allRoutesJSON);
+
+                if (DataProvider.allRoutes !== undefined && DataProvider.allRoutes != null) JsonDataStorage.pushAllRoutes(DataProvider.allRoutesJSON);
+                DataProvider.allRoutesLoaded = true;
+                console.log("Routes loaded from server.");
+            }
+            else {
+                DataProvider.allRoutes = JSON.parse(DataProvider.allRoutesJSON);
+                DataProvider.allRoutesLoaded = true;
+                console.log("Routes loaded from localStorage.");
+            }
+        }
+
+        if(!DataProvider.allTimetablesLoaded){
+            if (DataProvider.allTimetablesJSON == null) {
+                console.log("Downloading timetables from server...");
+
+                let response = await fetch(ApiConfig.apiGetTimetablesUrl);
+                DataProvider.allTimetablesJSON = await response.text();
+                DataProvider.allTimetables = JSON.parse(DataProvider.allTimetablesJSON);
+
+                if (DataProvider.allTimetables !== undefined && DataProvider.allTimetables != null) JsonDataStorage.pushAllTimetables(DataProvider.allTimetablesJSON);
+                DataProvider.allTimetablesLoaded = true;
+                console.log("Timetables loaded from server.");
+            }
+            else {
+                DataProvider.allTimetables = JSON.parse(DataProvider.allTimetablesJSON);
+                DataProvider.allTimetablesLoaded = true;
+                console.log("Timetables loaded from localStorage.");
+            }
+        }
     }
 }
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// End load data.
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-export default DataProvider;
