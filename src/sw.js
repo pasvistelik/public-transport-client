264f012304f8ccca2f5a71a17fbfb2768fb1823f
pasvistelik <pasvistelik@gmail.com>
2017-09-16 20:01:43 +0300

    upd

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@ import OptimalRoutesCollection from 'public-transport-find-optimal-ways/lib/opti
 console.log('Hello from SW...');
 
 
-const CACHE_NAME = 'mosm-app-v1';
+const CACHE_NAME = 'my-cache';
 
 const urlsToCache = [
   '/',
@@ -17,81 +17,115 @@ const urlsToCache = [
   //'/sw.js'
 ];
 
+// Install event - cache files (...or not)
+// Be sure to call skipWaiting()!
 self.addEventListener('install', function(event) {
   console.log('ServiceWorker installed.');
-  // Perform install steps
-  /*event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-  event.waitUntil(self.skipWaiting());*/
-  /*event.waitUntil((async function(){
-    let cache = await caches.open(CACHE_NAME);
-    console.log('install: opened cache');
-    await cache.addAll(urlsToCache);
-    console.log('install: added all urls to cache');
-    self.skipWaiting();
-  })());*/
-
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function(cache) {
-      console.log('Opened cache');
-      self.skipWaiting();
-      return cache.addAll(urlsToCache);
+	caches.open(CACHE_NAME).then(function(cache) {
+        // Important to `return` the promise here to have `skipWaiting()`
+        // fire after the cache has been updated.
+        return cache.addAll(urlsToCache);
+    }).then(function() {
+      // `skipWaiting()` forces the waiting ServiceWorker to become the
+      // active ServiceWorker, triggering the `onactivate` event.
+      // Together with `Clients.claim()` this allows a worker to take effect
+      // immediately in the client(s).
+      return self.skipWaiting();
     })
   );
-  //event.waitUntil(self.skipWaiting());
-
-
-
-  /*event.waitUntil(self.skipWaiting().then(function(){
-    caches.open(CACHE_NAME)
-    .then(function(cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    });
-  }))*/
-
 });
 
-
-/*self.addEventListener('install', function(event) {
-  console.log('ServiceWorker installed.');
-
-  var cachePromise = caches.open(CACHE_NAME).then(function(cache) {
-    return cache.addAll(urlsToCache);
-  })
-*/
-  /*// Perform install steps
-  var cachePromise = caches.open(CACHE_NAME)
-    .then(function(cache) {
-      console.log('install: opened cache');
-      return cache.addAll(urlsToCache);
-    })
-    .then(() => {
-      console.log('install: added all urls to cache');
-    });*/
-
-  //event.waitUntil(cachePromise);
-  //event.waitUntil(self.skipWaiting()); // Activate worker immediately
-//});
-
+// Activate event
+// Be sure to call self.clients.claim()
 self.addEventListener('activate', function(event) {
   console.log('ServiceWorker activated.');
-
-  //await DataProvider.loadDataAndInitialize();
-  
-  //event.waitUntil(self.clients.claim()); // Become available to all pages
-  
-  event.waitUntil(self.clients.claim().then(async function(){
-    await DataProvider.loadDataAndInitialize();
-  }));
-
+	// `claim()` sets this worker as the active worker for all clients that
+	// match the workers scope and triggers an `oncontrollerchange` event for
+	// the clients.
+	return self.clients.claim();
 });
+
+// self.addEventListener('install', function(event) {
+//   console.log('ServiceWorker installed.');
+//   // Perform install steps
+//   /*event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+//   event.waitUntil(self.skipWaiting());*/
+//   /*event.waitUntil((async function(){
+//     let cache = await caches.open(CACHE_NAME);
+//     console.log('install: opened cache');
+//     await cache.addAll(urlsToCache);
+//     console.log('install: added all urls to cache');
+//     self.skipWaiting();
+//   })());*/
+
+//   caches.open(CACHE_NAME)
+//   .then(function(cache) {
+//     console.log('Opened cache');
+//     return cache.addAll(urlsToCache);
+//   });
+//   /*event.waitUntil(
+//     caches.open(CACHE_NAME)
+//     .then(function(cache) {
+//       console.log('Opened cache');
+//       self.skipWaiting();
+//       return cache.addAll(urlsToCache);
+//     })
+//   );*/
+//   event.waitUntil(self.skipWaiting());
+
+
+
+//   /*event.waitUntil(self.skipWaiting().then(function(){
+//     caches.open(CACHE_NAME)
+//     .then(function(cache) {
+//       console.log('Opened cache');
+//       return cache.addAll(urlsToCache);
+//     });
+//   }))*/
+
+// });
+
+
+// /*self.addEventListener('install', function(event) {
+//   console.log('ServiceWorker installed.');
+
+//   var cachePromise = caches.open(CACHE_NAME).then(function(cache) {
+//     return cache.addAll(urlsToCache);
+//   })
+// */
+//   /*// Perform install steps
+//   var cachePromise = caches.open(CACHE_NAME)
+//     .then(function(cache) {
+//       console.log('install: opened cache');
+//       return cache.addAll(urlsToCache);
+//     })
+//     .then(() => {
+//       console.log('install: added all urls to cache');
+//     });*/
+
+//   //event.waitUntil(cachePromise);
+//   //event.waitUntil(self.skipWaiting()); // Activate worker immediately
+// //});
+
+// self.addEventListener('activate', function(event) {
+//   console.log('ServiceWorker activated.');
+
+//   //await DataProvider.loadDataAndInitialize();
+  
+//   //event.waitUntil(self.clients.claim()); // Become available to all pages
+  
+//   event.waitUntil(self.clients.claim().then(async function(){
+//     await DataProvider.loadDataAndInitialize();
+//   }));
+
+// });
 
 var clients = [];
 
