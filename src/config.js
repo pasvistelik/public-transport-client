cf80d80a2c776c42db1157aa11d7f64edb7fac80
pasvistelik <pasvistelik@gmail.com>
2017-11-08 13:22:19 +0300

    Change config.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-    apiPublicTransportServer: "https://public-transport-api-server.herokuapp.com/",
-    apiGetStationsUrl: "https://publictransportproject.000webhostapp.com/new/json/stations.json?v=2", //"stations"
-    apiGetRoutesUrl: "https://publictransportproject.000webhostapp.com/new/json/routes.json", //"routes"
-    apiGetTimetablesUrl: "https://publictransportproject.000webhostapp.com/new/json/timetables.json?v=3", //"timetables"
+    apiPublicTransportServer: "https://optimalway.github.io/",
+    apiGetStationsUrl: "https://optimalway.github.io/json/stations.json?v=10", //"stations"
+    apiGetRoutesUrl: "https://optimalway.github.io/json/routes.json?v=10", //"routes"
+    apiGetTimetablesUrl: "https://optimalway.github.io/json/timetables.json?v=10", //"timetables"
 
     clientVsSwNoKillingMessageInterval: 30000
 };
